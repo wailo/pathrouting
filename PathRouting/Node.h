1894380bd506d7e7271e7ba4bf02b6fe1a2959ef
wailo <wailo_2@hotmail.com>
2017-06-07 08:05:28 +0200

    PathRouting
    - Reidentation.

--- PathRouting/Node.h
+++ PathRouting/Node.h
@@ -1,69 +1,67 @@
 #pragma once
-//#include <Windows.h>
 #include "P3.h"
 
 class QuadTree;
 
 class Node
 {
+
 public:
-		enum nodeType
-	{
-		ROOT_TYPE,
-		NODE_TYPE,
-		LEAF_TYPE
-	};
 
-	// Node type
-	nodeType type;
-
-	// Node ID
-	int id;
-
-	// Node depth 
-	short depth;
-
-	// XYZ point
-	//P3 centre;
-
-	// parent Tree
-	QuadTree* m_parentTree;
-
-	// Calculate this node displacement value, which is the distance from centre point to the edge of node, same as raduis
-	double x_dsp();
-	double y_dsp();
-
-	// Calculate node displacement value at given depth. Displacement is the distance from centre point to the edge of node, same as raduis
-	double x_dsp( int depth );
-	double y_dsp( int depth );
-
-	double centre_x();
-	double centre_y();
-	double centre_z();
+  enum nodeType
+  {
+    ROOT_TYPE,
+    NODE_TYPE,
+    LEAF_TYPE
+  };
 
 
-	// Pointer to childern nodes
-	Node *Child[4];
+  Node(QuadTree* p_parentTree);
+  ~Node(void);
 
-	// Pointer to parent node
-	Node *Parent;
+  // Node type
+  nodeType type;
 
-	// Node counter
-	static unsigned int nodecount;
+  // Node ID
+  int id;
 
-	// Path routing
+  // Node depth 
+  short depth;
 
-	// A* algorithm parent
-	Node *A_Parent;
+  // parent Tree
+  QuadTree* m_parentTree;
 
-	// A* Cost
-	double cost;
-	double f_cost;
+  // Calculate this node displacement value, which is the distance from centre point to the edge of node, same as raduis
+  double x_dsp();
+  double y_dsp();
 
-	// Boolean to indicate when a node need to be redrawn. 
-	bool m_draw;
+  // Calculate node displacement value at given depth. Displacement is the distance from centre point to the edge of node, same as raduis
+  double x_dsp( int depth );
+  double y_dsp( int depth );
 
-	Node(QuadTree* p_parentTree);
-	~Node(void);
+  double centre_x();
+  double centre_y();
+  double centre_z();
+
+  // Pointer to childern nodes
+  Node *Child[4];
+
+  // Pointer to parent node
+  Node *Parent;
+
+  // Node counter
+  static unsigned int nodecount;
+
+  // Path routing
+
+  // A* algorithm parent
+  Node *A_Parent;
+
+  // A* Cost
+  double cost;
+  double f_cost;
+
+  // Boolean to indicate when a node need to be redrawn. 
+  bool m_draw;
+
 };
-
